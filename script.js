4d1799d1b95916808257f4018c481fbcfc2863e1
Murka1456 <mal-bod2@hotmail.com>
2025-05-15 13:01:23 +0200

    script

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ console.log(userPresent);
 const firstName = "Julie";
 const lastName = "Hauge";
 const fullName = firstName + "" + lastName + "";
-const wholeName = ${firstName} ${lastName}!;
+const wholeName = `${firstName} ${lastName}!`;
 console.log(fullName);
 console.log(wholeName);
 function bussinessCard(){
-constfirstName = "Julie";
+const firstName = "Julie";
 console.log(firstName);
-
+//Sett opp  en variabel for lastname
+const lastName = "Hauge";
+//sett opp en variabl for fullName
+const fullName = firstName + " "+ lastName;
+//Logg fullName til console
+console.log(fullName);
+//Mer om dett tirsdag 19.05
+const body = document.querySelector("h1")
+body.textContent = fullName;
 }
 bussinessCard();
