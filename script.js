1fdf90e535946cc84101d41fa0b9fbdf23760e80
Murka1456 <mal-bod2@hotmail.com>
2025-05-15 11:34:23 +0200

    java script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello World!");
+let userName = "JUlie";
+console.log(userName);
+userName = "Julius";
+console.log(userName);
+let userAge =28;
+console.log(userAge);
+let userPresent =true;
+console.log(userPresent);
+const firstName = "Julie";
+const lastName = "Hauge";
+const fullName = firstName + "" + lastName + "";
+const wholeName = ${firstName} ${lastName}!;
+console.log(fullName);
+console.log(wholeName);
+function bussinessCard(){
+constfirstName = "Julie";
+console.log(firstName);
+
+}
+bussinessCard();
