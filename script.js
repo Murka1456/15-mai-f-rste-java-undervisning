fc2b410ec9527baf25f88090c2e42bf7af0322a6
Murka1456 <mal-bod2@hotmail.com>
2025-05-20 10:21:30 +0200

    id

--- script.js
+++ script.js
@@ -16,14 +16,16 @@ console.log(wholeName);
 function bussinessCard(){
 const firstName = "Julie";
 console.log(firstName);
-//Sett opp  en variabel for lastname
-const lastName = "Hauge";
-//sett opp en variabl for fullName
-const fullName = firstName + " "+ lastName;
-//Logg fullName til console
-console.log(fullName);
-//Mer om dett tirsdag 19.05
-const body = document.querySelector("h1")
-body.textContent = fullName;
+
 }
 bussinessCard();
+//Regular function
+function hellowFunction(){
+    console.log("Hellow from inside function");
+} 
+ hellowFunction();
+ //
+ //
+ function clickButton(){
+    
+ }
